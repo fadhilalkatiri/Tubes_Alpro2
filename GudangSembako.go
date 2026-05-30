fbf4190a5e99952cd2d73fcf893ca7fa5417071c
fadhilalkatiri <fadhilalialkatiri1204@gmail.com>
2026-05-30 22:34:59 +0700

    Create GudangSembako.go

---
+++ GudangSembako.go
@@ -0,0 +1,84 @@
+package main
+import "fmt"
+
+const NMAX = 1000
+
+type barang struct {
+	kode, nama string
+	stok, harga int
+}
+
+type gudang[NMAX]barang
+
+func main() {
+
+}
+
+func menu() {
+
+}
+
+
+
+func tambahBarang() {
+
+}
+
+func tampilBarang() {
+
+}
+
+func cariBarangSequential() {
+
+}
+
+func cariBarangBinary() {
+
+}
+
+func editBarang() {
+
+}
+
+func hapusBarang() {
+
+}
+
+func barangMasuk() {
+
+}
+
+func barangKeluar() {
+
+}
+
+func menuSorting() {
+
+}
+
+func selectionSortStokAsc() {
+
+}
+
+func selectionSortHargaDesc() {
+
+}
+
+func insertionSortNamaAsc() {
+
+}
+
+func insertionSortNamaDesc() {
+
+}
+
+func sequentialSearch(x string) int {
+	return -1
+
+}
+
+func binarySearch(x string) int {
+	return -1
+
+}
+
